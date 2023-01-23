4f02e55f796f7af7c859f6c28e3ecd02c733dfd2
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-23 09:48:10 -0300

    Tipos de Dados

---
+++ 01-modulo-basico/tipos.rb
@@ -0,0 +1,16 @@
+# Tipos em Ruby
+produto = 'Macbook Air'
+preco = 5999.99
+quantidade = 1
+disponivel = true
+
+puts produto # Macbook Air
+puts preco # 5999.99
+puts quantidade # 1
+puts disponivel # true
+
+puts '|---- tipos ----|'
+puts produto.class # String
+puts preco.class # Floar
+puts quantidade.class # Integer
+puts disponivel.class # TrueClass
