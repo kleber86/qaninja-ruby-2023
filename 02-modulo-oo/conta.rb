a62816adda2fadab8eeff877fe6bfe5a3f8cc8b5
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-24 03:19:14 -0300

    Construtores

--- 02-modulo-oo/conta.rb
+++ 02-modulo-oo/conta.rb
@@ -1,13 +1,18 @@
 class Conta
-  attr_accessor :saldo
+  attr_accessor :saldo, :nome
 
+  def initialize(nome)
+    self.saldo = 0.0
+    self.nome = nome
+  end
   def deposita(valor)
     self.saldo += valor
-    puts "Depositando a quantia de #{valor} reais."
+    puts "Depositando a quantia de #{valor} reais na conta de #{self.nome}."
   end
 end
 
-c = Conta.new
-c.saldo = 0.0
+c = Conta.new('Kleber')
 c.deposita 100.00
-puts c.saldo
+puts c.saldo # 100.0
+c.deposita(10.00)
+puts c.saldo # 110.0
