50778943ef586870152e29ee5fd3e39395705264
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-26 19:06:30 -0300

    Validando o resultado esperado

--- 03-modulo-tdd/spec/avengers_spec.rb
+++ 03-modulo-tdd/spec/avengers_spec.rb
@@ -17,5 +17,44 @@ describe AvengersHeadQuarter do
 
     hq.put('Spiderman')
     expect(hq.list).to eql ['Spiderman']
+    expect(hq.list).to include 'Spiderman'
+  end
+
+  it 'deve adicionar uma lista de vingadores' do
+    hq = AvengersHeadQuarter.new
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+
+    res = hq.list.size > 0
+
+    expect(hq.list).to include 'Thor'
+    # expect(hq.list.size).to be > 0
+    expect(res).to be true
+    expect(hq.class).to eql AvengersHeadQuarter
+  end
+
+  it 'thor deve ser o primeiro da lista' do
+    hq = AvengersHeadQuarter.new
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+
+    expect(hq.list).to start_with('Thor')
+  end
+
+  it 'spiderman deve ser o ultimo da lista' do
+    hq = AvengersHeadQuarter.new
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+
+    expect(hq.list).to end_with('Spiderman')
+  end
+
+  it 'deve conter o sobrenome' do
+    avenger = 'Peter Paker'
+    expect(avenger).to match(/Paker/)
+    expect(avenger).not_to match('Alves')
   end
 end
