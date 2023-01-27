9607d40da1e5e065a7bf5bd971e11a08583430c0
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-27 10:54:54 -0300

    TDD na Prática

--- 03-modulo-tdd/spec/saque_spec.rb
+++ 03-modulo-tdd/spec/saque_spec.rb
@@ -1,12 +1,18 @@
 class ContaCorrente
-  attr_accessor :saldo
+  attr_accessor :saldo, :mensagem
 
   def initialize(saldo)
     @saldo = saldo
   end
 
   def saca(valor)
-    @saldo -= valor
+    if(valor > saldo)
+      @mensagem = 'Saldo insuficiente para saque :('
+    elsif (valor > 700.00)
+      @mensagem = 'Limite maxima por saque é de R$ 700'
+    else
+      @saldo -= valor
+    end
   end
 end
 
@@ -23,6 +29,51 @@ describe ContaCorrente do
         expect(@conta.saldo).to eql 800.00
       end
     end
-  end
 
-end
+    context 'quando o saldo é zero' do
+      before(:all) do
+        @conta = ContaCorrente.new(0.00)
+        @conta.saca(100.00)
+      end
+
+      it 'vejo a mensagem' do
+        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+      end
+
+      it('meu saldo permanece com zero') do
+        expect(@conta.saldo).to eql 0.00
+      end
+    end
+
+    context 'quando o saldo é insuficiente' do
+      before(:all) do
+        @conta = ContaCorrente.new(500.00)
+        @conta.saca(501.00)
+      end
+
+      it 'vejo a mensagem' do
+        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+      end
+
+      it('meu saldo permanece conforme o valor inicial') do
+        expect(@conta.saldo).to eql 500.00
+      end
+    end
+
+    context 'quando o valor do saque é maior que o permitido por transação' do
+      before(:all) do
+        @conta = ContaCorrente.new(1000.00)
+        @conta.saca(701.00)
+      end
+
+      it 'vejo a mensagem' do
+        expect(@conta.mensagem).to eql 'Limite maxima por saque é de R$ 700'
+      end
+
+      it('meu saldo permanece conforme o valor inicial') do
+        expect(@conta.saldo).to eql 1000.00
+      end
+    end
+
+  end # describe saque
+end # describe ContaCorrente
