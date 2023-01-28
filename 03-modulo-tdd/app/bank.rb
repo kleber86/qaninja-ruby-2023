1478a3ea84e7b78633be43c2636fa38267ab1f4a
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-28 01:06:13 -0300

    Herança e Conta Poupança

--- 03-modulo-tdd/app/bank.rb
+++ 03-modulo-tdd/app/bank.rb
@@ -1,17 +1,32 @@
-class ContaCorrente
+class Conta
   attr_accessor :saldo, :mensagem
 
   def initialize(saldo)
     @saldo = saldo
   end
 
-  def saca(valor)
+  def saca(valor, max, taxa)
     if(valor > saldo)
       @mensagem = 'Saldo insuficiente para saque :('
-    elsif (valor > 700.00)
-      @mensagem = 'Limite maxima por saque é de R$ 700'
+    elsif (valor > max)
+      @mensagem = 'Limite maxima por saque é de R$ ' + max.to_s
     else
-      @saldo -= valor
+      @saldo -= valor + taxa
     end
+  end
+
+end
+
+class ContaCorrente < Conta
+
+  def saca(valor, max = 700, taxa = 2.00)
+    super
+  end
+end
+
+class ContaPoupanca < Conta
+
+  def saca(valor, max = 500, taxa = 1.00)
+    super
   end
 end
--- 03-modulo-tdd/spec/bank/saque_cc_spec.rb
+++ 03-modulo-tdd/spec/bank/saque_cc_spec.rb
@@ -1,61 +1,61 @@
-require_relative '../app/bank'
+require_relative '../../app/bank'
 
 describe ContaCorrente do
 
   describe 'Saque' do
     context 'quando o valor é positivo' do
       before(:all) do
-        @conta = ContaCorrente.new(1000.00)
-        @conta.saca(200.00)
+        @cc = ContaCorrente.new(1000.00)
+        @cc.saca(200.00)
       end
 
       it 'então atualiza saldo' do
-        expect(@conta.saldo).to eql 800.00
+        expect(@cc.saldo).to eql 798.00
       end
     end
 
     context 'quando o saldo é zero' do
       before(:all) do
-        @conta = ContaCorrente.new(0.00)
-        @conta.saca(100.00)
+        @cc = ContaCorrente.new(0.00)
+        @cc.saca(100.00)
       end
 
       it 'então exibe a mensagem' do
-        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+        expect(@cc.mensagem).to eql 'Saldo insuficiente para saque :('
       end
 
       it('e o saldo final com zeros') do
-        expect(@conta.saldo).to eql 0.00
+        expect(@cc.saldo).to eql 0.00
       end
     end
 
     context 'quando o saldo é insuficiente' do
       before(:all) do
-        @conta = ContaCorrente.new(500.00)
-        @conta.saca(501.00)
+        @cc = ContaCorrente.new(500.00)
+        @cc.saca(501.00)
       end
 
       it 'então exibe a mensagem' do
-        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+        expect(@cc.mensagem).to eql 'Saldo insuficiente para saque :('
       end
 
       it('e o saldo permanece') do
-        expect(@conta.saldo).to eql 500.00
+        expect(@cc.saldo).to eql 500.00
       end
     end
 
     context 'quando supera o limite de saque' do
       before(:all) do
-        @conta = ContaCorrente.new(1000.00)
-        @conta.saca(701.00)
+        @cc = ContaCorrente.new(1000.00)
+        @cc.saca(701.00)
       end
 
       it 'então exibe a mensagem' do
-        expect(@conta.mensagem).to eql 'Limite maxima por saque é de R$ 700'
+        expect(@cc.mensagem).to eql 'Limite maxima por saque é de R$ 700'
       end
 
       it('e o saldo permanece') do
-        expect(@conta.saldo).to eql 1000.00
+        expect(@cc.saldo).to eql 1000.00
       end
     end
 
